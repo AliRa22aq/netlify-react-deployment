ea6dca63436e2d865fe5311606c1b614681b3280
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2021-01-11 00:43:10 +0500

    second serverless function added name: ali

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import './App.css';
 function App() {
 
   const [val, setVal] = useState({});
+  const [val2, setVal2] = useState({});
 
   useEffect(() => {
     fetch(`/.netlify/functions/hello?name=from Serverless Function`)
     .then(res => res.json())
     .then(obj => setVal(obj))
 
+  }, []);
+
+  useEffect(() => {
+    fetch(`/.netlify/functions/ali?name=is going to be a Millionaire`)
+    .then(res => res.json())
+    .then(obj => setVal2(obj))
+
   }, [])
   
   return (
     <div className="App">
-        {val.message}
+        {val.message} <br />
+        {val2.message}
     </div>
   );
 }
