32f2c143851bf3e5ea10d3ca3fa041eb4e70f3e9
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2021-01-11 00:30:23 +0500

    Hello serverless function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [val, setVal] = useState({});
+
+  useEffect(() => {
+    fetch(`/.netlify/functions/hello?name=from Serverless Function`)
+    .then(res => res.json())
+    .then(obj => setVal(obj))
+
+  }, [])
+  
   return (
     <div className="App">
-          Hello World! <br />
-          I love you
+        {val.message}
     </div>
   );
 }
